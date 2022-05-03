f8b09b826b09119062b6a6d40b6a4acecebc8236
meeyoungchoi <goodlife1359@gmail.com>
2022-05-03 23:19:44 +0900

    트랜잭션 예제1 - 계좌이체 - 이체가 잘되었는지 검증 - member 객체의 memberId값을 사용하여 데이터베이스에서 조회해 온다 - memberA의 잔액이 이체한 money 금액만큼 차감되었는지 검사 - memberB의 잔액이 이체받은 money 금액만큼 증가되었는지 검사

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,31 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepositoryV1;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작
+        Member fromMember = memberRepositoryV1.findById(fromId);
+        Member toMember = memberRepositoryV1.findById(toId);
+
+        memberRepositoryV1.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV1.update(toId, toMember.getMoney() + money);
+        // 트랜잭션 커밋 또는 롤백
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}
